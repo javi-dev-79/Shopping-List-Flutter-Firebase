4c5d79552f789a30a2f2bed16dba690cab84ad39
javi-dev-79 <javi.dev.79@gmail.com>
2023-12-20 16:54:50 +0000

    edit product screen was changed

--- lib/edit_product_screen.dart
+++ lib/edit_product_screen.dart
@@ -1,8 +1,182 @@
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:logger/logger.dart';
+// import 'constants.dart';
+// import 'product.dart';
+// import 'utils.dart';
+
+// class EditProductScreen extends StatefulWidget {
+//   final Product product;
+
+//   const EditProductScreen({Key? key, required this.product}) : super(key: key);
+
+//   @override
+//   _EditProductScreenState createState() => _EditProductScreenState();
+// }
+
+// class _EditProductScreenState extends State<EditProductScreen> {
+//   final TextEditingController _nameController = TextEditingController();
+//   final TextEditingController _categoryController = TextEditingController();
+//   final TextEditingController _priceController = TextEditingController();
+//   final TextEditingController _quantityController = TextEditingController();
+
+//   final Logger logger = Logger();
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Llena los controladores de texto con los datos del producto actual
+//     _nameController.text = widget.product.name;
+//     _categoryController.text = widget.product.category;
+//     _priceController.text = widget.product.price.toStringAsFixed(2);
+//     _quantityController.text = widget.product.quantity.toString();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Editar Producto'),
+//         backgroundColor: Colors.orange,
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             TextField(
+//               controller: _nameController,
+//               decoration: const InputDecoration(
+//                 labelText: 'Nombre',
+//                 labelStyle: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.orange,
+//                 ),
+//                 focusedBorder: UnderlineInputBorder(
+//                   borderSide: BorderSide(color: Colors.orange),
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 20),
+//             TextField(
+//               controller: _categoryController,
+//               decoration: const InputDecoration(
+//                 labelText: 'Categoría',
+//                 labelStyle: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.orange,
+//                 ),
+//                 focusedBorder: UnderlineInputBorder(
+//                   borderSide: BorderSide(color: Colors.orange),
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 20),
+//             TextField(
+//               controller: _priceController,
+//               keyboardType: TextInputType.number,
+//               decoration: const InputDecoration(
+//                 labelText: 'Precio',
+//                 labelStyle: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.orange,
+//                 ),
+//                 focusedBorder: UnderlineInputBorder(
+//                   borderSide: BorderSide(color: Colors.orange),
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 20),
+//             TextField(
+//               controller: _quantityController,
+//               keyboardType: TextInputType.number,
+//               decoration: const InputDecoration(
+//                 labelText: 'Cantidad',
+//                 labelStyle: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.orange,
+//                 ),
+//                 focusedBorder: UnderlineInputBorder(
+//                   borderSide: BorderSide(color: Colors.orange),
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: () async {
+//                 final String name = _nameController.text;
+//                 final String category = _categoryController.text;
+//                 final String priceText = _priceController.text;
+//                 final String quantityText = _quantityController.text;
+
+//                 if (name.isEmpty || category.isEmpty) {
+//                   return;
+//                 }
+
+//                 final double? price = Utils.parseDouble(priceText);
+//                 final int? quantity = Utils.parseInt(quantityText);
+
+//                 if (price == null ||
+//                     price <= 0 ||
+//                     quantity == null ||
+//                     quantity <= 0) {
+//                   return;
+//                 }
+
+//                 final updatedProduct = Product(
+//                   id: widget.product.id,
+//                   name: name,
+//                   category: category,
+//                   price: price,
+//                   quantity: quantity,
+//                   selected: widget.product.selected,
+//                 );
+
+//                 updateProductInList(updatedProduct);
+
+//                 Navigator.pop(context, updatedProduct);
+//               },
+//               style: ButtonStyle(
+//                 backgroundColor: MaterialStateProperty.all(Colors.orange),
+//               ),
+//               child: const Text('Guardar Cambios'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Future<http.Response> updateProductInList(Product product) async {
+//     final url = Uri.parse('http://$host:3000/products/${product.id}');
+
+//     logger.i('El id del producto es: ${product.id}');
+
+//     // Convierte el precio y la cantidad a números si son cadenas
+//     final double price = product.price is String
+//         ? double.parse(product.price as String)
+//         : product.price;
+//     final int quantity = product.quantity is String
+//         ? int.parse(product.quantity as String)
+//         : product.quantity;
+
+//     final response = await http.put(url, body: {
+//       'name': product.name,
+//       'category': product.category,
+//       'price': price.toString(),
+//       'quantity': quantity.toString(),
+//       'selected': product.selected.toString(),
+//     });
+
+//     return response;
+//   }
+// }
+
+// *****************************************************************************
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:firebase_database/firebase_database.dart';
 import 'package:logger/logger.dart';
-import 'constants.dart';
 import 'product.dart';
 import 'utils.dart';
 
@@ -16,6 +190,9 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+
+  late BuildContext _screenContext; // Almacena una referencia al contexto
+
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _categoryController = TextEditingController();
   final TextEditingController _priceController = TextEditingController();
@@ -23,14 +200,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   final Logger logger = Logger();
 
+  final DatabaseReference _productRef =
+      FirebaseDatabase.instance.ref().child('products');
+
   @override
   void initState() {
     super.initState();
-    // Llena los controladores de texto con los datos del producto actual
     _nameController.text = widget.product.name;
     _categoryController.text = widget.product.category;
     _priceController.text = widget.product.price.toStringAsFixed(2);
     _quantityController.text = widget.product.quantity.toString();
+
+    _screenContext = context;
   }
 
   @override
@@ -133,7 +314,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   selected: widget.product.selected,
                 );
 
-                updateProductInList(updatedProduct);
+                await _updateProductInList(updatedProduct);
 
                 Navigator.pop(context, updatedProduct);
               },
@@ -148,27 +329,32 @@ class _EditProductScreenState extends State<EditProductScreen> {
     );
   }
 
-  Future<http.Response> updateProductInList(Product product) async {
-    final url = Uri.parse('http://$host:3000/products/${product.id}');
+ Future<Product?> _updateProductInList(Product product) async {
+    final DatabaseReference productRef =
+        FirebaseDatabase.instance.ref().child('products').child(product.id.toString());
 
-    logger.i('El id del producto es: ${product.id}');
+    final double price = product.price;
+    final int quantity = product.quantity;
 
-    // Convierte el precio y la cantidad a números si son cadenas
-    final double price = product.price is String
-        ? double.parse(product.price as String)
-        : product.price;
-    final int quantity = product.quantity is String
-        ? int.parse(product.quantity as String)
-        : product.quantity;
-
-    final response = await http.put(url, body: {
+    await productRef.update({
       'name': product.name,
       'category': product.category,
-      'price': price.toString(),
-      'quantity': quantity.toString(),
-      'selected': product.selected.toString(),
+      'price': price,
+      'quantity': quantity,
+      'selected': product.selected,
     });
 
-    return response;
+    // Devuelve el producto actualizado
+    return product;
+  }
+
+    // Método para manejar la actualización y cerrar la pantalla
+  Future<void> _handleUpdate(Product updatedProduct) async {
+    final result = await _updateProductInList(updatedProduct);
+    if (result != null) {
+      // Usa el contexto local para cerrar la pantalla
+      Navigator.pop(_screenContext, result);
+    }
+    
   }
 }
