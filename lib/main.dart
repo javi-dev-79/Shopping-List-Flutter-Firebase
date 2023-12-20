db575f6202451d352419cfce8a57ef3faa709ae9
javi-dev-79 <javi.dev.79@gmail.com>
2023-12-20 15:26:07 +0000

    Classes main, registration, list_users, add_product and login were implemented with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,55 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'bottom_nav_bar.dart';
+// import 'package:firebase_core/firebase_core.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized(); // Asegúrate de que Flutter esté inicializado
+
+//   await dotenv.load(
+//       fileName: ".env"); // Carga las variables de entorno desde el archivo .env
+
+//   await Firebase.initializeApp(); // Inicializa Firebase
+
+//   runApp(
+//     MaterialApp(
+//       theme: ThemeData(
+//         unselectedWidgetColor:
+//             Colors.orange, // Color del checkbox no seleccionado
+//         // Otros ajustes de estilo que desees aplicar a toda la aplicación
+//       ),
+//       home: const MyApp(),
+//       debugShowCheckedModeBanner: false,
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: MyBottomNavigationBar(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+
+// ****************************************************************************
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'bottom_nav_bar.dart';
+import 'package:firebase_core/firebase_core.dart'; // Importa el paquete de Firebase
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Asegúrate de que Flutter esté inicializado
   await dotenv.load(
-      fileName: ".env"); // Carga las variables de entorno desde el archivo .env
+    fileName: ".env",
+  ); // Carga las variables de entorno desde el archivo .env
+  await Firebase.initializeApp(); // Inicializa Firebase
 
   runApp(
     MaterialApp(
@@ -30,4 +75,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 
--- lib/registration_screen.dart
+++ lib/registration_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
 
-import 'constants.dart';
+// import 'constants.dart';
 import 'list_users_screen.dart';
+
+import 'package:firebase_database/firebase_database.dart';
 
 class RegistrationScreen extends StatelessWidget {
   final TextEditingController _registrationTextEditingController =
@@ -14,24 +16,40 @@ class RegistrationScreen extends StatelessWidget {
 
   final Color _registrationCursorColor = Colors.orange;
 
-  final FocusNode _usuarioFocusNode = FocusNode();
+  final FocusNode _emailFocusNode = FocusNode();
 
   final Logger logger = Logger();
 
+  final DatabaseReference dbRef = FirebaseDatabase.instance.ref();
+
   RegistrationScreen({Key? key}) : super(key: key);
 
-  Future<http.Response> registerUser(String name, String email) async {
-    // final url = Uri.parse(
-    //     'http://$host:3000/users'); // Reemplaza con la URL de tu servidor JSON
-    final url =
-        Uri.parse('http://$host:3000/users'); // Actualiza la URL a "db.json"
+  // Future<http.Response> registerUser(String email, String pass) async {
+  //   // final url = Uri.parse(
+  //   //     'http://$host:3000/users'); // Reemplaza con la URL de tu servidor JSON
+  //   final url =
+  //       Uri.parse('http://$host:3000/users'); // Actualiza la URL a "db.json"
 
-    final response = await http.post(url, body: {
-      'name': name,
-      'email': email,
-    });
+  //   final response = await http.post(url, body: {
+  //     'email': email,
+  //     'pass': pass,
+  //   });
 
-    return response;
+  //   return response;
+  // }
+
+  Future<void> registerUser(String email, String pass) async {
+    try {
+      await dbRef.child('users').push().set({
+        'email': email,
+        'password': pass,
+      });
+      // Registro exitoso
+      logger.d('Usuario registrado exitosamente');
+    } catch (e) {
+      // Manejo de errores
+      logger.e('Error al registrar el usuario: $e');
+    }
   }
 
   @override
@@ -50,7 +68,7 @@ class RegistrationScreen extends StatelessWidget {
               controller: _registrationTextEditingController,
               cursorColor: _registrationCursorColor,
               decoration: const InputDecoration(
-                labelText: 'Usuario',
+                labelText: 'Email',
                 labelStyle: TextStyle(
                     fontWeight: FontWeight.bold, color: Colors.orange),
                 focusedBorder: UnderlineInputBorder(
@@ -65,7 +83,7 @@ class RegistrationScreen extends StatelessWidget {
               controller: _registrationEmailEditingController,
               cursorColor: _registrationCursorColor,
               decoration: const InputDecoration(
-                labelText: 'Email',
+                labelText: 'Contraseña',
                 labelStyle: TextStyle(
                     fontWeight: FontWeight.bold, color: Colors.orange),
                 focusedBorder: UnderlineInputBorder(
@@ -76,29 +94,29 @@ class RegistrationScreen extends StatelessWidget {
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                final name = _registrationTextEditingController.text;
-                final email = _registrationEmailEditingController.text;
+                final email = _registrationTextEditingController.text;
+                final pass = _registrationEmailEditingController.text;
 
-                final response = await registerUser(name, email);
+                await registerUser(email, pass);
 
-                if (response.statusCode == 201) {
-                  logger.d('Usuario registrado con éxito');
-                  // Navegar de nuevo a la pantalla de inicio de sesión después de un registro exitoso
-                  // Navigator.of(context).push(
-                  //   MaterialPageRoute(
-                  //     builder: (context) => LoginScreen(),
-                  //   ),
-                  // );
-                } else {
-                  logger.e(
-                      'Error al registrar el usuario. Código de estado: ${response.statusCode}');
-                }
+                // if (response.statusCode == 201) {
+                //   logger.d('Usuario registrado con éxito');
+                //   // Navegar de nuevo a la pantalla de inicio de sesión después de un registro exitoso
+                //   // Navigator.of(context).push(
+                //   //   MaterialPageRoute(
+                //   //     builder: (context) => LoginScreen(),
+                //   //   ),
+                //   // );
+                // } else {
+                //   logger.e(
+                //       'Error al registrar el usuario. Código de estado: ${response.statusCode}');
+                // }
                 // Borra los campos TextField
                 _registrationTextEditingController.clear();
                 _registrationEmailEditingController.clear();
 
                 // Enfoca nuevamente el campo 'Usuario'
-                _usuarioFocusNode.requestFocus();
+                _emailFocusNode.requestFocus();
               },
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(Colors.orange),
